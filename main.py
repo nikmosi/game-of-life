62d597115ea17e7a7db79ef660d872205cf2cc62
nikmosi <i@nikmosi.ru>
2025-03-12 20:40:58 +0700

    chore: create entry point with example

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from game_printer import GamePrinter
+from game_state import GameState
+
+
 def main():
-    print("Hello from game-of-life!")
+    start_state = [[False for _ in range(10)] for _ in range(10)]
+    start_state[6][3] = True
+    start_state[7][3] = True
+    start_state[7][2] = True
+    start_state[8][3] = True
+    start_state[8][4] = True
+    game = GameState(state=start_state)
+    for _ in range(10_000):
+        GamePrinter(game, "#", ".").print()
+        input("")
+        game = game.next()
 
 
 if __name__ == "__main__":
