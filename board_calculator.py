a41c4a9b8763b3f4a951bf5b45a7dd59112ed557
nikmosi <i@nikmosi.ru>
2025-03-29 00:51:39 +0700

    refactor(board_calculator): improve style

--- board_calculator.py
+++ board_calculator.py
@@ -1,36 +1,61 @@
-from copy import deepcopy
 from itertools import product
 
 
 class BoardCalculator:
+    """
+    A class to calculate the next state of the game board.
+    """
+
     def __init__(self, board: list[list[bool]]) -> None:
+        """
+        Initializes the BoardCalculator with the current state of the board.
+
+        :param board: The current state of the game board.
+        """
         self.board = board
         self.row_len = len(board)
+        self.col_len = len(board[0]) if self.row_len > 0 else 0
 
     def calc(self) -> list[list[bool]]:
-        new_state = deepcopy(self.board)
+        """
+        Calculates the next state of the board based on the current state.
 
-        for x, row in enumerate(self.board):
-            for y, is_alive in enumerate(row):
-                neighbors_count = self._count_neighbors(x, y)
-                new_state[x][y] = self._apply_rules(neighbors_count, is_alive)
-
+        :return: The next state of the board.
+        """
+        new_state = [
+            [
+                self._apply_rules(self._count_neighbors(x, y), cell)
+                for y, cell in enumerate(row)
+            ]
+            for x, row in enumerate(self.board)
+        ]
         return new_state
 
     def _apply_rules(self, neighbors_count: int, is_alive: bool) -> bool:
+        """
+        Applies the rules of the game to determine if a cell should be alive or dead.
+
+        :param neighbors_count: The number of live neighbors around the cell.
+        :param is_alive: The current state of the cell (alive or dead).
+        :return: The new state of the cell (alive or dead).
+        """
         if is_alive:
             return neighbors_count in [2, 3]
-        else:
-            return neighbors_count == 3
+        return neighbors_count == 3
 
     def _count_neighbors(self, x: int, y: int) -> int:
+        """
+        Counts the number of live neighbors around a given cell.
+
+        :param x: The x-coordinate of the cell.
+        :param y: The y-coordinate of the cell.
+        :return: The number of live neighbors around the cell.
+        """
         count = 0
         for i, j in product([-1, 0, 1], repeat=2):
-            if i == j and i == 0:
+            if i == 0 and j == 0:
                 continue
-            line_len = len(self.board[x])
-            row = (i + x + self.row_len) % self.row_len
-            ind = (j + y + line_len) % line_len
-            count += self.board[row][ind]
-
+            row = (x + i) % self.row_len
+            col = (y + j) % self.col_len
+            count += self.board[row][col]
         return count
